9d01cfe2e8a106f5d614a36ca1a76c8319f0e3db
Jack Ketcham <kcajketcham@gmail.com>
2012-11-18 00:27:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheHerald::Application.config.secret_token = 'fd492a1f1ae906a1f23994d5358457d4f756b89696355ecbe50461b978dbe59c4b64a16f2068da892ed65375cbffe998326ceedf22ae161d0ca2eb121d6577e0'
